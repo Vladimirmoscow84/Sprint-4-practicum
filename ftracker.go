e20243d15fbbe64cd82386ba79f50ccb23d0726a
Vladimirmoscow84 <ccr1@yandex.ru>
2024-09-10 15:39:03 +0300

    очередная проверка

--- ftracker.go
+++ ftracker.go
@@ -2,7 +2,6 @@ package ftracker
 
 import (
 	"fmt"
-	"math"
 )
 
 // Основные константы, необходимые для расчетов.
@@ -102,19 +101,7 @@ const (
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
 	// ваш код здесь
 	speedInSec := meanSpeed(action, duration) * kmhInMsec //скорость в метрах в секунду.
-	fmt.Println("Средняя скорость в секунду =", speedInSec)
-	k1 := walkingCaloriesWeightMultiplier * weight
-	fmt.Println("K1 =", k1)
-	k2 := math.Pow(speedInSec, 2)
-	fmt.Println("k2 = ", k2)
-	k3 := k2 / height
-	fmt.Println("k3 = ", k3)
-	k4 := walkingSpeedHeightMultiplier * weight
-	fmt.Println("k4 = ", k4)
-	k5 := duration * minInH
-	fmt.Println("k5 = ", k5)
-	fmt.Println("total: (k1 + k3) * k4 * k5 = ", (k1+k3)*k4*k5)
-	return ((walkingCaloriesWeightMultiplier*weight + (math.Pow(speedInSec, 2)/height)*walkingSpeedHeightMultiplier*weight) * duration * minInH)
+	return (walkingCaloriesWeightMultiplier*weight + ((speedInSec*speedInSec)/height)*walkingSpeedHeightMultiplier*weight) * duration * minInH
 }
 
 // Константы для расчета калорий, расходуемых при плавании.
